83e18f679ef527b87c5df0a541df53334239d3b2
J R Deva Dattan <jrdevadattan2006@gmail.com>
2026-01-02 14:44:06 +0530

    favicon and logo change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.svg';
 import userAvatar from '../assets/user_avatar.svg';
 import ReportPopup from './ReportPopup';
 
